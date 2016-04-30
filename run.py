190199fb03f73ac02625052f559893f5de780f53
Andres <paezand@gmail.com>
2016-04-30 15:11:32 -0700

    twilio

--- run.py
+++ run.py
@@ -1,20 +1,39 @@
 #!venv/bin/python
-from twilio.rest import TwilioRestClient
-import twilio
+import os
+from flask import Flask, request
+from twilio.util import TwilioCapability
 import twilio.twiml
-from flask import Flask, request, redirect
-from app import app
-app.run(debug=True)
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-    """Respond with a simple text message."""
+@app.route('/token')
+def token():
+  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
+  auth_token = os.environ.get("AUTH_TOKEN", AUTH_TOKEN)
+  app_sid = os.environ.get("APP_SID", APP_SID)
 
-    resp = twilio.twiml.Response()
-    resp.message("Hello! This is a test msg.")
-    return str(resp)
+  capability = TwilioCapability(account_sid, auth_token)
+
+  # This allows outgoing connections to TwiML application
+  if request.values.get('allowOutgoing') != 'false':
+     capability.allow_client_outgoing(app_sid)
+
+  # This allows incoming connections to client (if specified)
+  client = request.values.get('client')
+  if client != None:
+    capability.allow_client_incoming(client)
+
+  # This returns a token to use with Twilio based on the account and capabilities defined above
+  return capability.generate()
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+  resp = twilio.twiml.Response()
+  resp.say("Welcome to Twilio")
+  return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
