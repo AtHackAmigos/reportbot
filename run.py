67541206115058f0a8518b385413bc2c872ec0b4
Emily Lam <mail@emilymlam.com>
2016-04-30 16:13:56 -0700

    fix

--- run.py
+++ run.py
@@ -48,5 +48,5 @@ if __name__ == "__main__":
     connect(app)
     print "Connected to DB."
 
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
