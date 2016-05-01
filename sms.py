2b311bb96ce26d1e0c13145d782046150ebb1e99
mg <guajardom@gmail.com>
2016-05-01 13:18:17 -0700

    Refactor and add debug info.

--- sms.py
+++ sms.py
@@ -1,10 +1,11 @@
 #!venv/bin/python
 import twilio.twiml
-from model import db, phone_exists, Event, Registry
+from model import db, last_question_type, phone_exists, Event, Question, Registry
 
 
 def handle_sms_callback(request):
   from_number = request.values.get("From", None)
+  print "DEBUG: request.values: " + request.values
   if from_number is None:
   	resp = twilio.twiml.Response()
   	resp.message("Sorry, your number is unknown")
@@ -13,10 +14,17 @@ def handle_sms_callback(request):
   if not phone_exists(from_number):
   	return _register_new_user(from_number)
 
-  resp = twilio.twiml.Response()
-  resp.message("You have signed up already. " +
-    "Stay tunned for a follow up message next week :)")
-  return str(resp)
+  (is_pulse, answer_value_type) = last_question_type(from_number)
+
+  if is_pulse:
+  	_handle_weekly_checkup(from_number)
+
+  if answer_value_type == Question.VALUE_TYPE_UNKNOWN:
+  	resp = twilio.twiml.Response()
+    resp.message("You have signed up already. " +
+      "Stay tunned for a follow up message next week :)")
+    return str(resp)
+
 
 def _register_new_user(from_number):
   new_phone = Registry(phone=from_number)
@@ -30,3 +38,6 @@ def _register_new_user(from_number):
   resp.message("Welcome to ReportBot! (We will keep your number " +
   	from_number + " confidential.)")
   return str(resp)
+
+def _handle_weekly_checkup(from_number):
+  pass
