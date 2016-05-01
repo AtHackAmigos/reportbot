cc2ce7ed9b4ee4a8ab6bfb328cee9fd0facb00d8
mg <guajardom@gmail.com>
2016-05-01 11:02:06 -0700

    Extract registering new user.

--- sms.py
+++ sms.py
@@ -7,22 +7,29 @@ def phone_exists(phone):
   return bool(Registry.query.filter_by(phone=phone).first());
 
 def record_sms(db, request):
-  resp = twilio.twiml.Response()
   from_number = request.values.get("From", None)
   if from_number is None:
+  	resp = twilio.twiml.Response()
     resp.message("Sorry, your number is unknown")
     return str(resp)
 
   if not phone_exists(from_number):
-    new_phone = Registry(phone=from_number)
-    db.session.add(new_phone)
-    db.session.commit()
-    db.session.add(Event(phone=from_number, event_type=Event.EVENT_TYPE_HIRE))
-    db.session.commit()
-    resp.message("Welcome to ReportBot! (We will keep your number " +
-      from_number + " confidential.)")
-    return str(resp)
+  	return _register_new_user(from_number)
 
+  resp = twilio.twiml.Response()
   resp.message("You have signed up already. " +
     "Stay tunned for a follow up message next week :)")
+  return str(resp)
+
+def _register_new_user(from_number):
+  new_phone = Registry(phone=from_number)
+  db.session.add(new_phone)
+  # Need to commit *before* adding event since Registry is the key.
+  db.session.commit()
+
+  db.session.add(Event(phone=from_number, event_type=Event.EVENT_TYPE_HIRE))
+  db.session.commit()
+  resp = twilio.twiml.Response()
+  resp.message("Welcome to ReportBot! (We will keep your number " +
+  	from_number + " confidential.)")
   return str(resp)
